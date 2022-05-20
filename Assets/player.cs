84137d3fe1e022d0ae51d1ef0c2228d5ee6f4a2f
guf <rico.novanto907@gmail.com>
2022-05-20 15:38:36 +0700

    inital commit

---
+++ Assets/player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float kecepatan, scaleX, lompat;
+    private Animator anim;
+
+    void Start()
+
+    {
+
+        scaleX = transform.localScale.x;
+        anim = GetComponent<Animator>();
+    }
+
+
+
+    public void jalan_kiri()
+    {
+
+        transform.localScale = new Vector3(-scaleX, transform.localScale.y, transform.localScale.z);
+
+        transform.Translate(Vector3.left * kecepatan * Time.fixedDeltaTime, Space.Self);
+
+    }
+
+
+
+    public void jalan_kanan()
+    {
+
+        transform.localScale = new Vector3(-scaleX, transform.localScale.y, transform.localScale.z);
+
+        transform.Translate(Vector3.right * kecepatan * Time.fixedDeltaTime, Space.Self);
+
+    }
+
+
+
+    public void melompat()
+    {
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, lompat);
+
+    }
+
+
+
+    void Update()
+
+    {
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            jalan_kiri();
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            jalan_kanan();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            melompat();
+
+        }
+
+    }
+
+}
